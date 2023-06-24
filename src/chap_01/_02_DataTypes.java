4cd4fa1852f2d5374b54a1ac2a33c2429c237155
yerin03 <kyr2532@naver.com>
2023-06-24 22:03:06 +0900

    DataType

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,14 @@
+package chap_01;
+
+public class _02_DataTypes {
+
+    public static void main(String[] args) {
+
+        System.out.println(12);
+        System.out.println("Hi");
+        System.out.println(true);
+        System.out.println(3.14);
+
+        System.out.println(123 + 123);
+    }
+}
