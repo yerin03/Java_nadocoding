95c9c903a67f907679fcbde1abbe71839f5eb2d3
yerin03 <kyr2532@naver.com>
2023-06-24 22:18:38 +0900

    Namming

---
+++ src/chap_01/_05_VariableNaming.java
@@ -0,0 +1,20 @@
+package chap_01;
+
+public class _05_VariableNaming {
+    public static void main(String[] args) {
+
+        // 1.어울리는 이름
+        // 2. _,문자,숫자 사용 가능
+        // 3. 숫자로 시작 불가능
+        // 4. 1~2개이상의 단어의 연속
+        // 5. 소문자로 시작, 각 단어의 시작 글자는 대문자
+        // 6. 예약어 사용 불가
+
+        String nationality = "한국";
+
+        // 절대 변하지 않는 상수는 대문자로
+
+        String CODE = "KR";
+
+    }
+}
