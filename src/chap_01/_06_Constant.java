0108d42814f34eda34b39404ca6507c88d90812a
yerin03 <kyr2532@naver.com>
2023-06-24 22:22:23 +0900

    constant

---
+++ src/chap_01/_06_Constant.java
@@ -0,0 +1,12 @@
+package chap_01;
+
+public class _06_Constant {
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE  = "+82";
+        final double PI = 3.141592;
+        final String DATE_OF_BIRTH = "2000.03.03";
+        
+
+        System.out.println(KR_COUNTRY_CODE);
+    }
+}
