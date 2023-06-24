cdc5efc2ae17005a8ee070062298ffa51c48d247
yerin03 <kyr2532@naver.com>
2023-06-24 22:14:53 +0900

    Comment

--- src/chap_01/_04_Comment.java
+++ src/chap_01/_04_Comment.java
@@ -3,7 +3,8 @@ package chap_01;
 public class _04_Comment {
     public static void main(String[] args) {
 
-        
-
+        System.out.println("(10분 전) 결혼식 곧 시작하니까 착석 부탁드려요.");
+        //System.out.println("(5분 전) 결혼식 곧 시작하니까 착석 부탁드려요.");
+        System.out.println("지금부터 결혼식 시작할게요");
     }
 }
